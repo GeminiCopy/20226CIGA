424bad381b8532a71179913239bac8c089338842
SmallWoodIce <1633456990@qq.com>
2026-01-31 15:51:00 +0800

    第一关场景（未放置manager）

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class GameManager:SingletonAutoMono<GameManager>
 {
     
-    string resourcePath = "Prefeb/UI/GameOverPanel";//结束面板的路径
+    string EndResourcePath = "Prefebs/UI/GameOverPanel";//结束面板的路径
+    //角色路径
+    string PlayerOutPath = "Prefebs/Player/PlayerOut.prefab";
     GameObject GameOverPanelPrefab;//结束面板prefeb
     GameObject GameOverPanelPrefabInstance;//实例化的面板
-    public Vector3 RespawnPoint=Vector3.zero;//玩家重生时传送的位置,先用vector3占着，你们想修改成obj或者其他的都行
+    GameObject PlayerOut;
+    public GameObject RespawnPoint=null;//玩家重生时传送的位置,先用vector3占着，你们想修改成obj或者其他的都行
 
     public float RestartCounter, RestartCount;
     private bool isDead;
     private void Awake()
     {
         Init();
+        PlayerOut = transform.Find("PlayerOut").gameObject;
     }
     private void Update()
     {
@@ -31,7 +35,7 @@ public class GameManager:SingletonAutoMono<GameManager>
     {
         RestartCount = 2f;//重新开始时间
         RestartCounter = RestartCount;
-        GameObject GameOverPanelPrefab = ResourcesMgr.Instance.Load<GameObject>(resourcePath);//加载面板
+        GameObject GameOverPanelPrefab = ResourcesMgr.Instance.Load<GameObject>(EndResourcePath);//加载面板
     }
     //角色死亡时调用
     public void OnDie()
@@ -44,5 +48,10 @@ public class GameManager:SingletonAutoMono<GameManager>
         isDead = false;
         RestartCounter = RestartCount;//重置自动死亡时间
         //todo执行重生逻辑
+        if(RespawnPoint==null)
+        {
+            Debug.Log("重生错误");
+        }
+
     }
 }
