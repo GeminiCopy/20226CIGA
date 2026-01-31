ca8dc74b19f003a3d13a21b55c9a249934a0db82
OBSERVATER <3254140082@qq.com>
2026-01-31 18:31:53 +0800

    feat(对话): 对话系统气泡
    对话系统气泡

---
+++ Assets/Scripts/Controller/DialogActor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DialogActor : MonoBehaviour
+{
+    public string id; // target
+    public DialogController myBubble; 
+    private void Start()
+    {
+        // 注册自己
+        DialogManager.Inst.Register(id,myBubble);
+    }
+
+    private void OnDestroy()
+    {
+        // 反注册
+        if (DialogManager.Inst != null)
+        {
+            DialogManager.Inst.UnRegister(id);
+        }
+    }
+}
--- Assets/Scripts/Manager/DialogManager.cs
+++ Assets/Scripts/Manager/DialogManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
-using TMPro;
 using UnityEngine;
 
 [Serializable]
@@ -76,59 +75,5 @@ public class DialogManager : Singleton<DialogManager>
 
         _isPlaying = false;
         Debug.Log("Dialog Finished");
-    }
-}
-
-public class DialogController : MonoBehaviour
-{
-    [Header("UI Components")]
-    public Canvas canvas;          // 用于控制显示/隐藏
-    public TextMeshProUGUI tmp;    // 文本组件
-
-    // 内部状态
-    private bool _isTyping;
-    private bool _skipTyping;
-    private UniTaskCompletionSource _clickSignal;
-
-    private void Awake()
-    {
-        if (canvas != null) canvas.enabled = false;
-    }
-
-    // 简单的 LookAt 相机逻辑，防止气泡侧对着屏幕看不清
-    private void LateUpdate()
-    {
-        if (canvas.enabled && Camera.main != null)
-        {
-            transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
-        }
-    }
-
-    public async UniTask ShowDialogAsync(Dialog dialog)
-    {
-        if (canvas != null) canvas.enabled = true;
-        tmp.text = "";
-        
-        _isTyping = true;
-        _skipTyping = false;
-
-        foreach (char c in dialog.Content)
-        {
-            if (_skipTyping)
-            {
-                tmp.text = dialog.Content;
-                break;
-            }
-            tmp.text += c;
-            await UniTask.Delay(100);
-        }
-        _isTyping = false;
-        
-        await UniTask.WaitForSeconds(dialog.Time);
-    }
-
-    public void Close()
-    {
-        if (canvas != null) canvas.enabled = false;
     }
 }
