5449e28c641bf47d81f9cfc7cf48701b78d5cbcb
OBSERVATER <3254140082@qq.com>
2026-02-01 16:16:33 +0800

    feat(item): 添加物品描述
    添加物品描述

---
+++ Assets/Scripts/Manager/ItemDescVision.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ItemDescVision : MonoBehaviour
+{
+    public int id;
+    public TextMeshPro tmp;
+    private ItemDesc desc;
+    private void Awake()
+    {
+        desc = ItemManager.Inst.Get(id);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        tmp.gameObject.SetActive(true);
+        if (other.gameObject.name.Contains("Camera"))
+        {
+            tmp.text = desc.desc;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        tmp.gameObject.SetActive(false);
+    }
+}
