dfad1b27e8b78d3a8eff1e5294aec6941f1dec2c
SmallWoodIce <1633456990@qq.com>
2026-02-01 19:02:56 +0800

    场景二硬币

--- Assets/Scripts/Scene/Stage2.cs
+++ Assets/Scripts/Scene/Stage2.cs
@@ -6,6 +6,7 @@ public class Stage2 : MonoBehaviour
 {
     private void Start()
     {
+
         UIMgr.Instance.ShowPanel<BlankPanel>(callback: OnStage1Start);
         PlayerManager.Instance.player1=GameObject.Find("PlayerIn").GetComponent<PlayerController2D>();
         PlayerManager.Instance.player2 = GameObject.Find("PlayerOut").GetComponent<PlayerController2D>();
@@ -13,9 +14,9 @@ public class Stage2 : MonoBehaviour
     private void OnStage1Start(BlankPanel arg0)
     {
 
-      
-            CameraManager.Instance.AddNewSubCamera();
-            CameraManager.Instance.AddNewSubCamera();
+        CameraManager.Instance.Clear();
+        CameraManager.Instance.AddNewSubCamera();
+        CameraManager.Instance.AddNewSubCamera();
         GameObject.Find("BlankPanel").GetComponent<BasePanel>().HideMe();
     }
 }
